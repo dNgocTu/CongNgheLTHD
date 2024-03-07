f75cb2d76b101fec50ee2871496d7bf5d89cd15b
dNgocTu <tungoc4321@gmail.com>
2024-03-07 11:24:38 +0700

    initial project

---
+++ ecourseapp/courses/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+from cloudinary.models import CloudinaryField
+
+
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(BaseModel):
+    name = models.CharField(max_length=50)
+    description = RichTextField(null=True)
+    image = CloudinaryField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=50)
+    content = RichTextField(null=True)
+    image = CloudinaryField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+
